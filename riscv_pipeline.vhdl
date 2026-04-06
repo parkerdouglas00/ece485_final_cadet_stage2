4ec426ca5bcd54854d579a585ea0644ff67f7672
Parker Douglas <c26parker.douglas@afacademy.af.edu>
2026-04-05 19:29:00 -0600

    files updated to fix display_x5 not counting down past 8

--- riscv_pipeline.vhdl
+++ riscv_pipeline.vhdl
@@ -436,8 +436,8 @@ begin
     -- Comparator 
     not_equal_flag <= '1' when ex_mem_reg1_data /= ex_mem_reg2_data else '0';
     
-    next_pc <= std_logic_vector(signed(ex_mem_npc) + signed(ex_mem_imm)) when (ex_mem_branch = '1' and not_equal_flag = '1') else -- branch case
-               std_logic_vector(signed(ex_mem_npc) + signed(ex_mem_imm)) when (ex_mem_jump = '1') else  -- jump case
+    next_pc <= std_logic_vector(signed(NPC) + signed(ex_mem_imm)) when (ex_mem_branch = '1' and not_equal_flag = '1') else
+               std_logic_vector(signed(NPC) + signed(ex_mem_imm)) when (ex_mem_jump = '1') else
                NPC; -- note: this happens during IF !!! 1st two during MEM
                       
     -- MEM/WB pipeline register
